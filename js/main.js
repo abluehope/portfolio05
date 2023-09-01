2763386dd0a94bdac569d30270543d7647c268f8
abluehope <100765576+abluehope@users.noreply.github.com>
2023-09-01 12:21:22 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,45 @@
 $(function () {
+  $(".mobile_btn").on("click", function () {
+    $(this).toggleClass("on");
+    $(".gnb").toggleClass("on");
+  });
+
+  $(".gnb").on("wheel touchmove", function (e) {
+    e.preventDefault();
+  });
+
+  $(".gnb>ul>li>a").on("click", function (e) {
+    if ($(".gnb").hasClass("on")) {
+      e.preventDefault();
+    }
+    $(this).next().stop().slideToggle();
+    $(this).parent().siblings().find(".sub").slideUp();
+  });
+
+  $(".main_slide").on("init afterChange", function (e, s, c) {
+    const current = $(".main_slide .slick-current");
+    current.addClass("on").siblings().removeClass("on");
+
+    $(".main_visual .menu li").eq(0).addClass("on");
+    $(".main_visual .menu li")
+      .eq(c)
+      .addClass("on")
+      .siblings()
+      .removeClass("on");
+  });
+
   $(".main_slide").slick({
+    pauseOnHover: false,
     autoplay: true,
     arrows: false,
     autoplaySpeed: 6000,
     speed: 1000,
-    pauseOnHover: false,
+  });
+
+  $(".main_visual .menu li").on("click", function (e) {
+    e.preventDefault();
+    const idx = $(this).index(); // 0, 1, 2
+    $(".main_slide").slick("slickGoTo", idx);
   });
 
   $(".main_visual .arrows .left").on("click", function () {
@@ -19,6 +54,26 @@ $(function () {
     centerPadding: "0",
     arrows: false,
     slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "0",
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "0",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
   $(".main_special_offers .arrows .left").on("click", function () {
